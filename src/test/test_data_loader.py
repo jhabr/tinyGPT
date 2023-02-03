03ba6b856f974cc673cc16f2d34320b7673b42b2
Jaro Habr <jaro@freshbits.ch>
2023-02-03 14:30:15 +0100

    add datasets and batching

--- src/test/test_data_loader.py
+++ src/test/test_data_loader.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from src.tinyGPT.data_loader import DataLoader
+from src.tinyGPT.tokenizer import Tokenizer
 
 
 class DataLoaderTests(unittest.TestCase):
@@ -9,13 +10,41 @@ class DataLoaderTests(unittest.TestCase):
         self.data_loader = DataLoader(
             file_path=os.path.join(
                 os.getcwd(), "..", "..", "data", "tiny_shakespeare.txt"
-            )
+            ),
+            tokenizer=Tokenizer()
         )
+        self.data_loader.load_corpus()
 
     def tearDown(self) -> None:
         self.data_loader = None
 
     def test_load(self) -> None:
-        text = self.data_loader.load_corpus()
+        self.assertIsNotNone(self.data_loader.text)
 
-        self.assertIsNotNone(text)
+    def test_create_dataset(self) -> None:
+        train_dataset, valid_dataset = self.data_loader.create_datasets()
+
+        self.assertIsNotNone(train_dataset)
+        self.assertIsNotNone(valid_dataset)
+
+    def test_get_batch(self) -> None:
+        self.data_loader.create_datasets()
+        x, y = self.data_loader.get_batch(split="train")
+
+        # e.g.
+        # tensor([[52,  1, 58, 56, 47, 59, 51, 54],
+        #         [53, 58, 46, 47, 52, 45,  1, 40],
+        #         [53,  1, 57, 43, 58,  1, 53, 52],
+        #         [12,  1, 58, 53,  1, 58, 46, 43]])
+        self.assertEqual((4, 8), x.shape)
+
+        # e.g.
+        # tensor([[ 1, 58, 56, 47, 59, 51, 54, 46],
+        #         [58, 46, 47, 52, 45,  1, 40, 59],
+        #         [ 1, 57, 43, 58,  1, 53, 52, 43],
+        #
+        # if input x is [52], output y = [1]
+        # x: [52, 1] => y: 58
+        # x: [52, 1, 58] => y: 56
+        self.assertEqual((4, 8), y.shape)
+
--- src/tinyGPT/data_loader.py
+++ src/tinyGPT/data_loader.py
@@ -1,7 +1,90 @@
+from typing import Union
+
+import torch
+
+from src.tinyGPT.tokenizer import Tokenizer
+
+
 class DataLoader:
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: str, tokenizer: Tokenizer) -> None:
+        """
+        A data loader for the tinyGPT model.
+
+        Parameters:
+             file_path: str
+                The path to the file that will be used to train the model.
+            tokenizer: Tokenizer
+                The tokenizer that will be used to tokenize the text.
+
+        Returns:
+            None
+        """
         self.file_path = file_path
+        self.tokenizer = tokenizer
+        self.text = None
+        self.train_dataset = None
+        self.valid_dataset = None
 
     def load_corpus(self) -> str:
+        """
+        Loads the corpus from the file path.
+
+        Returns:
+            text: str
+                The text from the file.
+        """
         with open(self.file_path, "r") as file:
-            return file.read()
+            self.text = file.read()
+            return self.text
+
+    def create_datasets(
+        self, train_split_size: float = 0.9
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Creates the training and validation datasets.
+
+        Parameters:
+            train_split_size: float
+                The size of the training split. The validation split will be 1 - train_split_size.
+
+        Returns:
+            train_dataset: torch.Tensor
+                The training dataset.
+        """
+        tokens = self.tokenizer.encode(self.text)
+        data = torch.tensor(tokens, dtype=torch.long)
+
+        no_tokens = int(train_split_size * len(data))
+        self.train_dataset = data[:no_tokens]
+        self.valid_dataset = data[no_tokens:]
+
+        return self.train_dataset, self.valid_dataset
+
+    def get_batch(
+        self,
+        split: str = Union["train", "valid"],
+        block_size: int = 8,
+        batch_size: int = 4,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Returns the next batch of data.
+
+        Parameters:
+            split: str
+                The split of the data to use. Either "train" or "valid".
+            block_size: int
+                The size of the block of data to return.
+            batch_size: int
+                The size of the batch to return.
+
+        Returns:
+            batch: torch.Tensor
+                The batch of data.
+        """
+        data = self.train_dataset if split == "train" else self.valid_dataset
+        indices = torch.randint(high=len(data) - block_size, size=(batch_size,))
+        x = torch.stack([data[index : index + block_size] for index in indices])
+        y = torch.stack(
+            [data[index + 1 : index + block_size + 1] for index in indices]
+        )  # offset by 1 == target is the next character in the sequence
+        return x, y
