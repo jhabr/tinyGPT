28b11fa6982aace695033e2028c41a2d5d0c2eb3
Jaro Habr <jaro@freshbits.ch>
2023-02-04 16:46:46 +0100

    refactor device name to device type

--- src/tinyGPT/backend.py
+++ src/tinyGPT/backend.py
@@ -6,14 +6,14 @@ import torch.backends.mps
 class Backend:
     @staticmethod
     def device() -> torch.device:
-        device_name = "cpu"
+        device_type = "cpu"
 
         if Backend.is_mac_os() and torch.backends.mps.is_available():
-            device_name = "mps"
+            device_type = "mps"
         elif torch.cuda.is_available():
-            device_name = "cuda"
+            device_type = "cuda"
 
-        return torch.device(device_name)
+        return torch.device(device_type)
 
     @staticmethod
     def is_mac_os() -> bool:
